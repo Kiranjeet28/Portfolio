d918ca27476d8efd9a25158a1d3e9cf206d15a84
kiranjeet kour <kiranjeetkour144@gmail.com>
2024-05-20 16:45:12 +0530

    add new bg for sm

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -1,44 +1,30 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
-export default function Banner(){
-    const [data,useData]= useState([])
-    useEffect(()=>{
+import React, { useState, useEffect } from 'react';
+import { BackgroundGradientAnimation } from './ui/background-gradient-animation';
+import IBanner from './InnerComponents/IBanner';
 
-        fetch('https://api.github.com/users/kiranjeet28')
-        .then(response=>response.json())
-        .then(data=>{
-            useData(data)
-            
-        })
-    },[])
-    return(
-        // Add the different images for the medium and sm in it leter and add the twitter and linkedn Image
-        <div className=' dark:bg-black bg-gray-200  p-4 h-auto flex flex-col md:flex-wrap md:bg-no-repeat md:bg-cover md:bg-bg'>
-          <div className='flex md:flex-col '>
-                <img src={data.avatar_url} className=" rounded-[50%] p-0 m-0 h-28 md: max-w-48 md:relative md:self-center md:bottom-[-40px]" alt="Git D.p" />
+export default function Banner() {
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-                <div className='p-0 m-0  text-center' >
-                    <div className='text-black bold text-2xl font-bold font-mono p-0 m-0 md:relative md:bottom-32 md:text-3xl md:m-3  dark:text-white md:text-black md:dark:text-black'>Kiranjeet Kour</div>
-                    <p className=" text-black text-[10px] p-0 m-0 pb-2 md:text-[17px] md:font-semibold  font-bold shadow-black dark:text-white md:dark:text-black  overflow ">{data.bio}</p>
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 771); // Change threshold as needed
+        };
 
-                </div>
-          </div>
-         
-          <div className='flex justify-evenly'>
-            <a href="https://github.com/kiranjeet28">
-               <img src='https://cdn-1.webcatalog.io/catalog/github/github-icon.png' alt="" className='h-[4vh] rounded-md '/>
-            </a>
-            <a href="https://www.linkedin.com/in/kiranjeet-kour-3823ba268"> 
-            <img src='https://pngimg.com/uploads/linkedIn/linkedIn_PNG7.png' alt="" className='h-[4vh] rounded-md '/>
-            </a>
-            <a href="https://twitter.com/KourSuri5352"> 
-            <img src="https://th.bing.com/th/id/OIP.PjG-Z-6wXH8l-9kLJKLdqgAAAA?rs=1&pid=ImgDetMain" alt="" className='h-[3vh] rounded-md '/>
-            </a>
-            <a href="mailto:kiranjeetkour144@gmail.com"> 
-            <img src="https://logos-world.net/wp-content/uploads/2020/11/Gmail-Logo.png" alt="" className='h-[3vh] rounded-md '/>
-            </a>
-          </div>
-        
-          </div>
-    )
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    
+    return (
+        <div>
+        {isSmallScreen ? (
+            <BackgroundGradientAnimation>
+                <IBanner />
+            </BackgroundGradientAnimation>
+        ) : (
+            <IBanner />
+        )}
+    </div>
+    );    
 }
