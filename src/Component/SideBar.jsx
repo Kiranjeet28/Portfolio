1fdab547223832668cf6f3899be571beb66f363e
kiranjeet kour <kiranjeetkour144@gmail.com>
2024-05-21 00:11:59 +0530

    done

--- src/Component/SideBar.jsx
+++ src/Component/SideBar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Resume from "../assets/Resume.pdf"
 export  function SideBar({bg}){
     return(
         <div className="h-auto w-auto" id="Sidebar">
@@ -10,7 +9,8 @@ export  function SideBar({bg}){
                     <a href="https://www.linkedin.com/in/kiranjeet-kour-3823ba268/" className=""><div className=" text-gray-200   h-10 text-[12px]  border-t  border-gray-200  dark:text-gray-900 dark:border-gray-900  hover:bg-gray-300 hover:text-gray-900  dark:hover:bg-gray-800 dark:hover:text-gray-100 md:text-xl font-semibold   p-2 font-mono md:p-4 ">Linkedin</div></a>
                     <a href="https://www.instagram.com/kiranjeetkour2024/" className=""><div className=" text-gray-200   h-10 text-[12px]  border-t  border-gray-200  dark:text-gray-900 dark:border-gray-900  hover:bg-gray-300 hover:text-gray-900  dark:hover:bg-gray-800 dark:hover:text-gray-100 md:text-xl font-semibold   p-2 font-mono  md:p-4 ">Instagram</div></a>
                     <a href="https://twitter.com/KourSuri5352" className=""><div className=" text-gray-200   h-10 text-[12px]  border-t  border-gray-200  dark:text-gray-900 dark:border-gray-900  hover:bg-gray-300 hover:text-gray-900  dark:hover:bg-gray-800 dark:hover:text-gray-100 md:text-xl font-semibold   p-2 font-mono  md:p-4 border-b">Twitter</div></a>
-                    <a href={Resume} className=""><div className=" text-gray-200   h-10 text-[12px]  border-t  border-gray-200  dark:text-gray-900 dark:border-gray-900  hover:bg-gray-300 hover:text-gray-900  dark:hover:bg-gray-800 dark:hover:text-gray-100 md:text-xl font-semibold   p-2 font-mono  md:p-4 border-b">Resume</div></a>
+                     <a href="https://docs.google.com/presentation/d/1R66dmZUGwb0orpvGeHdi1L4iFpSUg3Jf/edit?usp=drive_link&ouid=100002198285189806065&rtpof=true&sd=true" className=""><div className=" text-gray-200   h-10 text-[12px]  border-t  border-gray-200  dark:text-gray-900 dark:border-gray-900  hover:bg-gray-300 hover:text-gray-900  dark:hover:bg-gray-800 dark:hover:text-gray-100 md:text-xl font-semibold   p-2 font-mono  md:p-4 border-b">CV</div></a>
+                    <a href="https://docs.google.com/document/d/1pb0JX-2L1E_oqYxZEMZ2y-kfsLL7xFf3/edit?usp=drive_link&ouid=100002198285189806065&rtpof=true&sd=true" className=""><div className=" text-gray-200   h-10 text-[12px]   border-gray-200  dark:text-gray-900 dark:border-gray-900  hover:bg-gray-300 hover:text-gray-900  dark:hover:bg-gray-800 dark:hover:text-gray-100 md:text-xl font-semibold   p-2 font-mono  md:p-4 border-b">Resume</div></a>
                     
                 </div>
         </div>
